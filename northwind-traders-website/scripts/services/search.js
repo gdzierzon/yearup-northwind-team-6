a06718095c0cd34bf0e61d24bdfc36643ff7c01d
VVSteven <21elvisc@gmail.com>
2023-12-08 14:42:15 -0700

    added code to the search.js

--- northwind-traders-website/scripts/services/search.js
+++ northwind-traders-website/scripts/services/search.js
@@ -1,12 +1,31 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    selects = document.getElementById("searchSelect")
-    selects.addEventListener("change",searchSelected)
-    }
-)
+let categoriesPage = "http://localhost:8081/api/categories"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selects = document.getElementById("searchSelect");
+  selects.addEventListener("change", searchSelected);
+});
 
 function searchSelected() {
-    selects = document.getElementById("searchSelect")
-    const selectedSearch = selects.value
-    console.log(selectedSearch)
+  const selects = document.getElementById("searchSelect");
+  const selectedSearch = selects.value;
+  console.log(selectedSearch);
+
+  if (selectedSearch === "1") {
+    loadCategories();
+  } else {
+    loadProducts();
+  }
+}
+function loadCategories(){
+    
 }
 
+function displayCategories() {
+  // Your code to display categories goes here
+  console.log("Displaying categories");
+}
+
+function displayProducts() {
+  // Your code to display products goes here
+  console.log("Displaying products");
+}
